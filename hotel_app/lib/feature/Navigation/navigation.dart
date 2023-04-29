c703450a15ec6d833cf8c9d72ee2a3e80c7a8bd3
Arivan Shamal <91335722+evan8133@users.noreply.github.com>
2023-04-29 20:58:50 +0300

    nav fix

--- hotel_app/lib/feature/Navigation/navigation.dart
+++ hotel_app/lib/feature/Navigation/navigation.dart
@@ -27,33 +27,45 @@ class _NavigationPagesState extends State<NavigationPages> {
       appBar: AppBar(
         title: const Text('Hotel Booking App'),
       ),
-      bottomNavigationBar: GNav(
-        backgroundColor: Theme.of(context).colorScheme.onPrimary,
-        activeColor: Theme.of(context).colorScheme.primary,
-        padding: const EdgeInsets.all(15),
-        gap: 5,
-        selectedIndex: index,
-        onTabChange: (value) {
-          setState(() {
-            index = value;
-          });
-        },
-        tabs: [
-          GButton(
-            icon: index == 0 ? Icons.home : Icons.home_outlined,
-            text: 'Home',
+      bottomNavigationBar: Container(
+        color: Theme.of(context).colorScheme.onPrimary,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 12,
           ),
-          GButton(
-            icon: index == 1 ? Icons.person : Icons.person_outlined,
-            text: 'Profile',
+          child: GNav(
+            backgroundColor: Theme.of(context).colorScheme.onPrimary,
+            activeColor: Theme.of(context).colorScheme.primary,
+            padding: const EdgeInsets.all(15),
+            gap: 5,
+            selectedIndex: index,
+            onTabChange: (value) {
+              setState(() {
+                index = value;
+              });
+            },
+            tabs: [
+              GButton(
+                icon: index == 0 ? Icons.home : Icons.home_outlined,
+                text: 'Home',
+              ),
+              GButton(
+                icon: index == 1 ? Icons.person : Icons.person_outlined,
+                text: 'Profile',
+              ),
+              GButton(
+                icon: index == 2 ? Icons.settings : Icons.settings_outlined,
+                text: 'Settings',
+              ),
+            ],
           ),
-          GButton(
-            icon: index == 2 ? Icons.settings : Icons.settings_outlined,
-            text: 'Settings',
-          ),
-        ],
+        ),
       ),
-      body: _screens[index],
+      body: SafeArea(
+        bottom: true,
+        child: _screens[index],
+      ),
     );
   }
 }

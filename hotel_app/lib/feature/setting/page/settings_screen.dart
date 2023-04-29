d2ab510a8027424e21620518a2fa4d50250b0979
Arivan Shamal <zaqiu190054@uniq.edu.iq>
2023-04-29 21:11:10 +0300

    setting UI

--- hotel_app/lib/feature/setting/page/settings_screen.dart
+++ hotel_app/lib/feature/setting/page/settings_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -12,8 +14,48 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Placeholder(),
+    return SettingsList(
+      sections: [
+        SettingsSection(
+          title: const Text('General'),
+          tiles: [
+            SettingsTile.navigation(
+              title: const Text('Abstract settings screen'),
+              leading: const Icon(CupertinoIcons.wrench),
+              description:
+                  const Text('UI created to show plugin\'s possibilities'),
+              onPressed: (context) {},
+            )
+          ],
+        ),
+        SettingsSection(
+          title: const Text('Application'),
+          tiles: [
+            SettingsTile.switchTile(
+              leading: const Icon(Icons.dark_mode_sharp),
+              title: const Text('Dark Mode'),
+              initialValue: true,
+              onToggle: (value) {},
+            ),
+          ],
+        ),
+        SettingsSection(
+          title: const Text('About'),
+          tiles: [
+            SettingsTile(
+              leading: const Icon(Icons.info_outline),
+              title: const Text('Terms of Service'),
+              onPressed: (context) {},
+            ),
+            SettingsTile(
+              leading: const Icon(Icons.info_outline),
+              title: const Text('Version'),
+              description: const Text('1.0.0'),
+              onPressed: (context) {},
+            ),
+          ],
+        ),
+      ],
     );
   }
 }

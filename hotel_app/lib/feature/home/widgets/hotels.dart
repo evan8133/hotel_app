d7f68bf0e8f4c65a35ea1b88009df5cbc4db95e6
Arivan Shamal <91335722+evan8133@users.noreply.github.com>
2023-05-26 22:09:36 +0300

    updates

--- hotel_app/lib/feature/home/widgets/hotels.dart
+++ hotel_app/lib/feature/home/widgets/hotels.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_iconpicker_plus/flutter_iconpicker.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HotelCard extends StatefulWidget {
@@ -28,104 +28,124 @@ class HotelCard extends StatefulWidget {
 class _HotelCardState extends State<HotelCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-            child: Image.network(
-              widget.url, // Replace with the actual image URL
-              height: 150,
-              width: double.infinity,
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        print('tab');
+      },
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(16)),
+              child: Image.network(
+                widget.url, // Replace with the actual image URL
+                height: 150,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.name,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  widget.address,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey[700],
-                  ),
-                ),
-                SizedBox(height: 8),
-                Row(
-                  children: List.generate(
-                    widget.star,
-                    (_) => Icon(
-                      Icons.star,
-                      color: Colors.yellow,
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  widget.description,
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  'Amenities:',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+                  const SizedBox(height: 8),
+                  Text(
+                    widget.address,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[700],
+                    ),
                   ),
-                ),
-                SizedBox(height: 8),
-                Wrap(
-                  spacing: 8,
-                  children: widget.amenities.map((amenity) {
-                    IconData iconData =
-                        MdiIcons.fromString(amenity) ?? Icons.question_mark;
-                    return Chip(
-                      label: Icon(iconData),
-                      backgroundColor: Colors.blue,
-                      labelStyle: TextStyle(color: Colors.white),
-                    );
-                  }).toList(),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  'Nearby Attractions:',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+                  const SizedBox(height: 8),
+                  RatingBar(
+                    initialRating: widget.star.toDouble(),
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    ignoreGestures: true,
+                    itemCount: 5,
+                    itemPadding: const EdgeInsets.symmetric(horizontal: 10),
+                    onRatingUpdate: (rating) {},
+                    ratingWidget: RatingWidget(
+                      full: const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      half: const Icon(
+                        Icons.star_half,
+                        color: Colors.amber,
+                      ),
+                      empty: const Icon(
+                        Icons.star_border,
+                        color: Colors.amber,
+                      ),
+                    ),
                   ),
-                ),
-                SizedBox(height: 8),
-                Wrap(
-                  spacing: 8,
-                  children: widget.nearbyAttractions.map((attraction) {
-                   //IconData iconData =
-                        //MdiIcons.fromString(attraction) ?? Icons.question_mark;
-                    return Chip(
-                      label: Text(attraction),
-                      backgroundColor: Colors.green,
-                      labelStyle: TextStyle(color: Colors.white),
-                    );
-                  }).toList(),
-                ),
-              ],
+                  const SizedBox(height: 16),
+                  Text(
+                    widget.description,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Amenities:',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Wrap(
+                    spacing: 8,
+                    children: widget.amenities.map((amenity) {
+                      IconData iconData =
+                          MdiIcons.fromString(amenity) ?? Icons.question_mark;
+                      return Chip(
+                        label: Icon(iconData),
+                        backgroundColor: Colors.blue,
+                        labelStyle: const TextStyle(color: Colors.white),
+                      );
+                    }).toList(),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Nearby Attractions:',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Wrap(
+                    spacing: 8,
+                    children: widget.nearbyAttractions.map((attraction) {
+                      //IconData iconData =
+                      //MdiIcons.fromString(attraction) ?? Icons.question_mark;
+                      return Chip(
+                        label: Text(attraction),
+                        backgroundColor: Colors.green,
+                        labelStyle: const TextStyle(color: Colors.white),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
